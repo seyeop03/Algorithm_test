b12a50db049d407598b4b6b966cae1f4359a3a87
seyeop <seyeop03@gmail.com>
2022-02-09 16:36:21 +0900

    Create BFS예제.py

---
+++ DFS+BFS/BFS예제.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+# BFS 메서드 정의
+def bfs(graph, start, visited):
+    # 큐(Queue) 구현을 위해 deque 라이브러리 사용
+    queue = deque([start])
+    # 현재 노드를 방문 처리
+    visited[start] = True
+    # 큐가 빌 때까지 반복
+    while queue:
+        # 큐에서 하나의 원소를 뽑아 출력
+        v = queue.popleft()
+        print(v, end=' ')
+        # 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+    
+# 각 노드가 연결된 정보를 리스트 자료형으로 표현(2차원 리스트)
+graph = [
+    [], # 통상적으로 코테 문제에서 노드1부터 시작하는 경우가 많으므로 0번째 인덱스는 비워둔다.
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7]
+]
+
+# 각 노드가 방문된 정보를 리스트 자료형으로 표현(1차원 리스트)
+visited = [False] * 9
+
+# 정의된 BFS 함수 호출
+bfs(graph, 1, visited)
